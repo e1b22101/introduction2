d1812b0e2d01be0d2ac7cadf4987216b88f0bb09
e1b22069 <e1b22069@oit.ac.jp>
2024-10-01 14:39:59 +0900

    リポジトリの初期化

---
+++ src/main/java/team6/introduction2/Introduction2Application.java
@@ -0,0 +1,13 @@
+package team6.introduction2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Introduction2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Introduction2Application.class, args);
+	}
+
+}
